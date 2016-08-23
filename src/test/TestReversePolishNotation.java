0b3e57c3af7b6c02d2d73739c35e375931ec4349
NellyHargeliya <nelly.hargeliy@gmail.com>
2016-08-23 15:00:15 +0300

    changed tests

--- src/test/TestReversePolishNotation.java
+++ src/test/TestReversePolishNotation.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.ExpressionParser;
 import main.ReversePolishNotation;
 import org.junit.Test;
 
@@ -8,19 +9,19 @@ import static org.junit.Assert.assertTrue;
 public class TestReversePolishNotation {
     @Test
     public void testGetExpression1() {
-        ReversePolishNotation rpn = new ReversePolishNotation();
+        ExpressionParser rpn = new ExpressionParser();
         assertTrue(rpn.getExpression("(1+2)*4+3").equals("1 2 + 4 * 3 + "));
     }
 
     @Test
     public void testGetExpression2() {
-        ReversePolishNotation rpn = new ReversePolishNotation();
+        ExpressionParser rpn = new ExpressionParser();
         assertTrue(rpn.getExpression("(8+2*5)/(1+3*2-4)").equals("8 2 5 * + 1 3 2 * 4 - + / "));
     }
 
     @Test
     public void testGetExpression3() {
-        ReversePolishNotation rpn = new ReversePolishNotation();
+        ExpressionParser rpn = new ExpressionParser();
         assertTrue(rpn.getExpression("(15/3+11-3*5)/3*(6-10)").equals("15 3 / 11 3 5 * - + 3 / 6 10 - * "));
     }
 
