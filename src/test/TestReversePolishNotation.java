1a7833989a37bd0efbdf1f9e62b9e4d44ecdd051
NellyHargeliya <nelly.hargeliy@gmail.com>
2016-08-24 16:57:42 +0300

    add changed

--- src/test/TestReversePolishNotation.java
+++ src/test/TestReversePolishNotation.java
@@ -28,19 +28,19 @@ public class TestReversePolishNotation {
     @Test
     public void testCalculate1() {
         ReversePolishNotation rpn = new ReversePolishNotation();
-        assertTrue(rpn.calculate("(1+2)*4+3") == 15);
+        assertTrue(rpn.calculatePolishNotation("(1+2)*4+3") == 15);
     }
 
     @Test
     public void testCalculate2() {
         ReversePolishNotation rpn = new ReversePolishNotation();
-        assertTrue(rpn.calculate("(8+2*5)/(1+3*2-4)") == 6);
+        assertTrue(rpn.calculatePolishNotation("(8+2*5)/(1+3*2-4)") == 6);
     }
 
     @Test
     public void testCalculate3() {
         ReversePolishNotation rpn = new ReversePolishNotation();
-        assertTrue(rpn.calculate("(15/3+11-3*5)/3*(6-10)") == 18);
+        assertTrue(rpn.calculatePolishNotation("(15/3+11-3*5)/3*(6-10)") == 18);
     }
 
 }
